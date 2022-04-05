4bc2aedb1ba22244faa1964f6c3a04fdcc9543c4
CHRis <christophervistal25@gmail.com>
2022-04-05 22:16:16 +0800

    Add unique validation of phone number

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -52,7 +52,7 @@ class RegisterController extends Controller
         return Validator::make($data, [
             'first_name' => ['required', 'string', 'max:255'],
             'last_name' => ['required', 'string', 'max:255'],
-            'phone_number' => ['required'],
+            'phone_number' => ['required', 'unique:users', 'regex:/((\+63)|0)\d{10}/'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
             'address' => ['required', 'max:255']
